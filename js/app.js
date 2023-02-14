f5e44ea076f208fdc021a91166a3372caf2e4f93
longo2002 <lombardo.fra2002@gmail.com>
2023-02-14 17:23:25 +0100

    milestone 2   bonus 1 done

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 console.log("works!")
 
-const ourTeam = {
-    "collega1" : ["Wayne", "Barnett",	"Founder & CEO",	"wayne-barnett-founder-ceo.jpg,"],
-    "collega2" : ["Angela", "Caroll",	"Chief Editor",	"angela-caroll-chief-editor.jpg,"],
-    "collega3" : ["Walter", "Gordon",	"Office Manager",	"walter-gordon-office-manager.jpg,"],
-    "collega4" : ["Angela", "Lopez",	"Social Media Manager",	"angela-lopez-social-media-manager.jpg,"],
-    "collega5" : ["Scott", "Estrada",	"Developer",	"scott-estrada-developer.jpg,"],
-    "collega6" : ["Barbara", "Ramos",	"Graphic Designer",	"barbara-ramos-graphic-designer.jpg,"],
-}
-  
+let container = document.getElementById("container")
+
+const ourTeam = [
+    ["Wayne", "Barnett", "Founder & CEO", "wayne-barnett-founder-ceo.jpg"],
+    ["Angela", "Caroll", "Chief Editor", "angela-caroll-chief-editor.jpg"],
+    ["Walter", "Gordon", "Office Manager", "walter-gordon-office-manager.jpg"],
+    ["Angela", "Lopez", "Social Media Manager", "angela-lopez-social-media-manager.jpg"],
+    ["Scott", "Estrada", "Developer", "scott-estrada-developer.jpg"],
+    ["Barbara", "Ramos", "Graphic Designer", "barbara-ramos-graphic-designer.jpg"]
+]
+
 console.log(ourTeam)
 
-for (let key in ourTeam){
-    console.log(ourTeam[key]);
+for (let i = 0; i < ourTeam.length ; i++) {
+    let divString = `<div class="cards">${ourTeam[i]}</div>`
+    let imgString = `<img src="./img/${ourTeam[i].slice(-1) }" alt="foto-collega">`
+    container.innerHTML += imgString + divString 
 }
