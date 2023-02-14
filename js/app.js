8a5fb4cf6d3e03cb4494ed34b689487302ad4d72
longo2002 <lombardo.fra2002@gmail.com>
2023-02-14 16:41:08 +0100

    milestone 0 done

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 console.log("works!")
 
 const ourTeam = {
-    "nome" : ["Wayne","Angela", "Walter", "Angela", "Scott", "Barbara"],
-    "cognome" : ["Barnett", "Caroll", "Gordon", "Lopez", "Estrada","Ramos"],
-    "ruolo" : ["Founder & CEO","Chief Editor","Office Manager","Social Media Manager","Developer","Graphic Designer"],
-    "foto" : ["wayne-barnett-founder-ceo.jpg","angela-caroll-chief-editor.jpg","walter-gordon-office-manager.jpg","angela-lopez-social-media-manager.jpg","scott-estrada-developer.jpg","barbara-ramos-graphic-designer.jpg"],
+    "collega1" : ["Wayne", "Barnett",	"Founder & CEO",	"wayne-barnett-founder-ceo.jpg,"],
+    "collega2" : ["Angela", "Caroll",	"Chief Editor",	"angela-caroll-chief-editor.jpg,"],
+    "collega3" : ["Walter", "Gordon",	"Office Manager",	"walter-gordon-office-manager.jpg,"],
+    "collega4" : ["Angela", "Lopez",	"Social Media Manager",	"angela-lopez-social-media-manager.jpg,"],
+    "collega5" : ["Scott", "Estrada",	"Developer",	"scott-estrada-developer.jpg,"],
+    "collega6" : ["Barbara", "Ramos",	"Graphic Designer",	"barbara-ramos-graphic-designer.jpg,"],
 }
   
 console.log(ourTeam)
 
+for (let key in ourTeam){
+    console.log(ourTeam[key]);
+}
