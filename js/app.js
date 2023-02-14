923f35a03bf982f6bf220d8ba049684de12e454b
longo2002 <lombardo.fra2002@gmail.com>
2023-02-14 16:33:46 +0100

    milestone 0 done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 console.log("works!")
 
-const ourTeam = [
+const ourTeam = {
+    "nome" : ["Wayne","Angela", "Walter", "Angela", "Scott", "Barbara"],
+    "cognome" : ["Barnett", "Caroll", "Gordon", "Lopez", "Estrada","Ramos"],
+    "ruolo" : ["Founder & CEO","Chief Editor","Office Manager","Social Media Manager","Developer","Graphic Designer"],
+    "foto" : ["wayne-barnett-founder-ceo.jpg","angela-caroll-chief-editor.jpg","walter-gordon-office-manager.jpg","angela-lopez-social-media-manager.jpg","scott-estrada-developer.jpg","barbara-ramos-graphic-designer.jpg"],
+}
   
-]
-
 console.log(ourTeam)
+
